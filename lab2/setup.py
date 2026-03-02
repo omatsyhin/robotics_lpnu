9714ca92017cbd473d4b605fba22072f4d38cf0c
Oleksandr Matsyhin <140163044+omatsyhin@users.noreply.github.com>
2026-03-02 15:48:19 +0000

    Update setup.py

--- lab2/setup.py
+++ lab2/setup.py
@@ -1,29 +1,35 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'lab2'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), 
+         glob(os.path.join('launch', '*launch.[pxy]*'))),
+        (os.path.join('share', package_name, 'worlds'), 
+         glob(os.path.join('worlds', '*.sdf'))),
+        (os.path.join('share', package_name, 'config'), 
+         glob(os.path.join('config', '*.rviz'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
+    maintainer='Your Name',
+    maintainer_email='your.email@lpnu.ua',
+    description='Lab 2: ROS2 Integration with Gazebo',
     license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'robot_controller = lab2.robot_controller:main',
+            'lidar_subscriber = lab2.lidar_subscriber:main',
         ],
     },
 )
